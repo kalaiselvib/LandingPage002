2cc4a17d58abf7852d1bc75f141dbc321f55fb14
kalaiselviEST <89083148+kalaiselviEST@users.noreply.github.com>
2022-06-22 16:42:54 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+   
+    // Screenshot carousel
+    $(".screenshot-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 500,
+        loop: true,
+        dots: true,
+        items: 1
+    });
+
+
+    
+})(jQuery);
+
